06b55b616db56e829ec96e141f00831de8585e76
Laura Hall <podinjshmi1993@gmail.com>
2023-09-20 10:02:00 +0100

    fixed the useEffect bug

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -35,6 +35,7 @@ export function NavBar({
 
     useEffect(() => {
         fetchAndStoreUser();
+        //eslint-disable-next-line react-hooks/exhaustive-deps
     }, [signedInUser]);
 
     return (
