bca41ca9ea12d513658b11d7ac2c841563f4c66c
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-09-26 21:59:37 +0100

    updated test to sign in, view study list and log out

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,22 +1,15 @@
 describe("Add a new resource", () => {
-    it("Signs in as a user, adds a resource and returns to the all resources page", () => {
+    it("Signs in as Adil, views his study list and logs out to return to all resources page", () => {
         cy.intercept("/users*").as("getUsers");
         cy.intercept("/resources*").as("getResources");
         cy.visit("http://localhost:3000//");
-        cy.wait(["@getResources", "@getUsers"]);
         cy.wait(1500);
+        cy.wait(["@getResources", "@getUsers"]);
         cy.get("select").select("Adil Rahman");
         cy.contains("Adil Rahman");
         cy.wait(1500);
-        cy.contains("Show more").click();
-        cy.wait(1500);
-        cy.contains("Show less").click();
-        cy.wait(1500);
-        cy.contains("Add new resource").click();
-        cy.wait(1500);
         cy.contains("View Study List").click();
-        cy.wait(1500);
-        cy.contains("View all Resources").click();
-        cy.wait(1500);
+        cy.wait(2500);
+        cy.contains("Logout").click();
     });
 });
