066578bd96174348d056c440f8f9ad2df0303dbb
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-09-26 16:28:25 +0100

    fixed cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+        viewportWidth: 1024,
+        viewportHeight: 768,
     },
 });
